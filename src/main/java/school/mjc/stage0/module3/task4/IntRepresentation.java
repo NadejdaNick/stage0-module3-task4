6ff2ed0f8a4111df81431a5f5b862d0f2b20d1c9
NadejdaNick <nhohlova2021@gmail.com>
2023-09-25 14:12:41 +0300

    changed

--- src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
+++ src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
@@ -2,5 +2,14 @@ package school.mjc.stage0.module3.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+
+        char first = 'c';
+        char second = 'h';
+        char third = 'a';
+        char forth = 'r';
+        int c = first - '0';
+        int h = second - '0';
+        int a = third - '0';
+        int r = forth - '0';
     }
 }
